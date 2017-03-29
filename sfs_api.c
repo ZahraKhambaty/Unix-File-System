9a8fe84e8352856be0cac657f5a51f19732aab90
Zahra Khambaty <zahrakhambaty@wpa148089.wireless.mcgill.ca>
2017-03-29 16:09:03 -0400

    Additional Structs-need initialization

--- sfs_api.c
+++ sfs_api.c
@@ -6,15 +6,14 @@
 
 typedef struct _superBlock_t {
     unsigned char magic [4];
+    int BLOCK_SIZE;
     int fileSysSize; //#blocks
     block_t blocks[1024];
-    inode_t root; // root is j node
-    inode_t shadow[4];
-    inode_t lastShadow;
+    inode_t root; // root is j node;
 } superBlock_t;
 
 typedef struct _inodeBlock_t {
-    inode_t inodes[16];
+    inode_t inodes[14];
 } inodeBlock_t;
 
 typedef struct _inode_t { //1 inode 64 bytes
@@ -32,11 +31,15 @@ typedef struct _rootDir_t { //root directory with 16 inodes/block
     char filenames[200];
 } rootDir_t;
 
-typedef struct _files_t{
+typedef struct _fileDescriptor_t{
+    inode_t* inode;
+    int inodeIndex;
     int readPointer;
     int writePointer;
 }files_t;
 
+files_t fileDescriptorTable_t[200];
+int inodeTracker[NUM_INODES];      //
 // initialize free bit map(FBM) and WriteMap(WM)
 
 const int SUPERBLOCK_NUM= 0;          // diskBlock where the super block is stored
@@ -46,10 +49,23 @@ const int  ROOTDIR_INODE_NUMBER = 1;  // hared coded inode number for the root d
 block_t FBM;
 block_t WM;
 
+void  init_superBlock_t(){
+    superBlock.magic = 4;
+    superBlock.fileSysSize= NUM_BLOCKS * BLOCK_SIZE; //#blocks
+    superBlock.block = NUM_BLOCKS;
+    superBlock.root= 0 ; // root is j node in the super block;
+} 
 
+void init_fileDescriptorTable_t(){
+    for (int i = 0;i< NUM_INODES;i++){
+        fileDescriptorTable_t[i] = -1; // means inode is not used.
+    }
+}
 
 void mkssfs(int fresh) {
     //OPEN A NEW DISK
+    init_fileDescriptorTable_t()
+    superBlock();
     if (fresh) {
         
         if (init_fresh_disk(FILE_NAME, BLOCK_SIZE, NUM_BLOCKS) == -1)
